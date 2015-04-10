5a6e209f5aa65f275165a396cab987a85de71205
RemyAndrey <kingsleyzheng0716@gmail.com>
2015-04-09 22:35:19 -0600

    GcmIntentService

---
+++ app/src/main/java/net/greenreceipt/greenreceipt/GCMIntentService.java
@@ -0,0 +1,92 @@
+package net.greenreceipt.greenreceipt;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class GCMIntentService extends IntentService {
+
+    public static final int notifyID = 9001;
+    public GCMIntentService() {
+        super("GcmIntentService");
+    }
+    NotificationCompat.Builder builder;
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public GCMIntentService(String name) {
+        super(name);
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty()) {
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR
+                    .equals(messageType)) {
+                sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED
+                    .equals(messageType)) {
+                sendNotification("Deleted messages on server: "
+                        + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE
+                    .equals(messageType)) {
+                sendNotification("Message Received from Google GCM Server:\n\n"
+                        + extras.get(ApplicationConstants.MSG_KEY));
+            }
+        }
+        NotificationReceiver.completeWakefulIntent(intent);
+    }
+
+    private void sendNotification(String msg) {
+        Intent resultIntent = new Intent(this, HomeActivity.class);
+        resultIntent.putExtra("msg", msg);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0,
+                resultIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder mNotifyBuilder;
+        NotificationManager mNotificationManager;
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("Alert")
+                .setContentText("You've received new message.")
+                .setSmallIcon(R.drawable.ic_launcher);
+        // Set pending intent
+        mNotifyBuilder.setContentIntent(resultPendingIntent);
+
+        // Set Vibrate, Sound and Light
+        int defaults = 0;
+        defaults = defaults | Notification.DEFAULT_LIGHTS;
+        defaults = defaults | Notification.DEFAULT_VIBRATE;
+        defaults = defaults | Notification.DEFAULT_SOUND;
+
+        mNotifyBuilder.setDefaults(defaults);
+        // Set the content for Notification
+        mNotifyBuilder.setContentText("New message from Server");
+        // Set autocancel
+        mNotifyBuilder.setAutoCancel(true);
+        // Post a notification
+        mNotificationManager.notify(notifyID, mNotifyBuilder.build());
+    }
+}
